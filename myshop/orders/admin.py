5e5ccb2ab5583f2b6b315bdc04611df00f28dc2d
bissaliev <bissaliev21@gmail.com>
2024-08-28 15:36:31 +0400

    В админку добавил action для скачивания списка заказов в формат CSV

--- myshop/orders/admin.py
+++ myshop/orders/admin.py
@@ -1,7 +1,42 @@
+import csv
+import datetime
+
 from django.contrib import admin
+from django.http import HttpResponse
 from django.utils.safestring import mark_safe
 
 from .models import Order, OrderItem
+
+
+def export_to_csv(modeladmin, request, queryset):
+    """
+    Административное действие по скачиванию списка заказов в формате CSV-файла.
+    """
+    opts = modeladmin.model._meta
+    content_disposition = f"attachment; filename={opts.verbose_name}.csv"
+    response = HttpResponse(content_type="text/csv")
+    response["Content-Disposition"] = content_disposition
+    writer = csv.writer(response)
+    fields = [
+        field
+        for field in opts.get_fields()
+        if not field.many_to_many and not field.one_to_many
+    ]
+    # записать первую строку с информацией заголовка
+    writer.writerow([field.verbose_name for field in fields])
+    # записать строки данных
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime("%d/%m/%Y")
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+
+
+export_to_csv.short_description = "Экспорт в CSV"
 
 
 def order_stripe_payment(obj):
@@ -41,3 +76,4 @@ class OrderAdmin(admin.ModelAdmin):
     ]
     list_filter = ["paid", "created", "updated"]
     inlines = [OrderItemInline]
+    actions = [export_to_csv]
