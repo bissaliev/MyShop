f24ddf02a722eb78bc65067387c93ee069264f26
bissaliev <bissaliev21@gmail.com>
2025-01-14 21:50:09 +0400

    Добавление документации и описание полей в модели orders

--- myshop/orders/models.py
+++ myshop/orders/models.py
@@ -8,16 +8,20 @@ from shop.models import Product
 
 
 class Order(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    email = models.EmailField()
-    address = models.CharField(max_length=250)
-    postal_code = models.CharField(max_length=20)
-    city = models.CharField(max_length=100)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    """Заказ"""
+
+    first_name = models.CharField("имя", max_length=50)
+    last_name = models.CharField("фамилия", max_length=50)
+    email = models.EmailField("электронный адрес")
+    address = models.CharField("адрес доставки", max_length=250)
+    postal_code = models.CharField("почтовый индекс", max_length=20)
+    city = models.CharField("город", max_length=100)
+    created = models.DateTimeField("время и дата создания", auto_now_add=True)
+    updated = models.DateTimeField("время и дата обновления", auto_now=True)
     paid = models.BooleanField("оплачен", default=False)
-    stripe_id = models.CharField(max_length=250, blank=True)
+    stripe_id = models.CharField(
+        "идентификатор платежа", max_length=250, blank=True
+    )
     coupon = models.ForeignKey(
         Coupon,
         related_name="orders",
@@ -33,6 +37,8 @@ class Order(models.Model):
     )
 
     class Meta:
+        verbose_name = "Заказ"
+        verbose_name_plural = "Заказы"
         ordering = ["-created"]
         indexes = [
             models.Index(fields=["-created"]),
@@ -41,28 +47,24 @@ class Order(models.Model):
     def __str__(self):
         return f"Order {self.id}"
 
-    def get_total_cost(self):
-        """
-        Метод получает общую стоимость товаров, приобретенных в этом заказе,
-        учитывать примененную скидку.
-        """
+    def get_total_cost(self) -> Decimal:
+        """Общая сумма заказа с учетом скидки"""
         total_cost = self.get_total_cost_before_discount()
         return total_cost - self.get_discount()
 
     def get_total_cost_before_discount(self):
+        """Общая сумма заказа без скидки"""
         return sum(item.get_cost() for item in self.items.all())
 
-    def get_discount(self):
+    def get_discount(self) -> Decimal:
+        """Скидочная сумма заказа"""
         total_cost = self.get_total_cost_before_discount()
         if self.discount:
             return total_cost * (self.discount / Decimal(100))
         return Decimal(0)
 
-    def get_stripe_url(self):
-        """
-        Метод используется для возврата URL-адреса информационной панели Stripe
-        для платежа, связанного с заказом.
-        """
+    def get_stripe_url(self) -> str:
+        """Получение URL-адреса информационной панели Stripe оплаты заказа"""
         if not self.stripe_id:
             # никаких ассоциированных платежей
             return ""
@@ -71,6 +73,8 @@ class Order(models.Model):
 
 
 class OrderItem(models.Model):
+    """Позиция товара в заказе"""
+
     order = models.ForeignKey(
         Order, related_name="items", on_delete=models.CASCADE
     )
@@ -80,12 +84,13 @@ class OrderItem(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveIntegerField("количество товаров", default=1)
 
+    class Meta:
+        verbose_name = "Позиция заказа"
+        verbose_name_plural = "Позиции заказа"
+
     def __str__(self):
         return str(self.id)
 
-    def get_cost(self):
-        """
-        Метод возвращает стоимость товара путем умножения цены товара
-        на количество.
-        """
+    def get_cost(self) -> Decimal:
+        """Получение общей суммы позиций определенного товара"""
         return self.price * self.quantity
