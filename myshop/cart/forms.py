cc6fdb21fb58bc98c6a6987b51fbfcf09d78cd2e
bissaliev <bissaliev21@gmail.com>
2025-01-30 08:08:15 +0400

    Добавил начальные значения в форму cart.forms.CartAddProductForm

--- myshop/cart/forms.py
+++ myshop/cart/forms.py
@@ -10,7 +10,7 @@ class CartAddProductForm(forms.Form):
     """
 
     quantity = forms.TypedChoiceField(
-        choices=PRODUCT_QUANTITY_CHOICES, coerce=int
+        choices=PRODUCT_QUANTITY_CHOICES, coerce=int, initial=1
     )
     override = forms.BooleanField(
         required=False, initial=False, widget=forms.HiddenInput
