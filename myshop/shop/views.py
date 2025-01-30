a6fded41a6d233fd7cfb740ad4787bd415796247
bissaliev <bissaliev21@gmail.com>
2025-01-30 08:06:17 +0400

    Обновление shop.views. Разделение классов на дополнительные миксины

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -1,22 +1,17 @@
+from cart.cart import Cart
 from cart.forms import CartAddProductForm
 from django.shortcuts import get_object_or_404
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import FormMixin
 from django.views.generic.list import ListView
+from shop.models import Category, Product
 
-from .models import Category, Product
 
-
-class ProductListView(ListView):
-    """Список товаров с фильтрацией по категориям"""
-
-    queryset = Product.objects.filter(available=True)
-    template_name = "shop/product/list.html"
+class CategoryFilterMixin:
+    """Миксин добавляющий фильтрацию товаров по категориям"""
 
     def get_context_data(self, **kwargs):
-        category = None
         context = super().get_context_data(**kwargs)
-        context["categories"] = Category.objects.all()
         category_slug = self.kwargs.get("category_slug")
         if category_slug:
             category = get_object_or_404(Category, slug=category_slug)
@@ -31,9 +26,36 @@ class ProductListView(ListView):
         return queryset
 
 
-class ProductDetailView(FormMixin, DetailView):
+class ProductListView(CategoryFilterMixin, ListView):
+    """Список товаров с фильтрацией по категориям"""
+
+    queryset = Product.objects.filter(available=True)
+    template_name = "shop/product/list.html"
+    paginate_by = 24
+
+
+class CartFormMixin(FormMixin):
+    """Миксин для добавления формы добавления товара в корзину"""
+
+    form_class = CartAddProductForm
+
+    def get_initial(self):
+        """
+        Устанавливаем начальные значения для формы,
+        в случае нахождения продукта в корзине,
+        """
+        initial = super().get_initial()
+        obj = self.get_object()
+        product_id = str(obj.id)
+        cart = Cart(self.request)
+        if product_id in cart:
+            initial["quantity"] = cart[product_id]["quantity"]
+            initial["override"] = True
+        return initial
+
+
+class ProductDetailView(CartFormMixin, DetailView):
     """Описание товара"""
 
     template_name = "shop/product/detail.html"
     queryset = Product.objects.filter(available=True)
-    form_class = CartAddProductForm
