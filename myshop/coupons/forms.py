a034ce6315aa6806bc515eebffc0b82605520385
bissaliev <bissaliev21@gmail.com>
2025-01-31 09:27:59 +0400

    coupons: Добавил плейсхолдер в форму CouponApplyForm

--- myshop/coupons/forms.py
+++ myshop/coupons/forms.py
@@ -6,4 +6,7 @@ class CouponApplyForm(forms.Form):
     Форма для ввода пользователем кода купона
     """
 
-    code = forms.CharField(label="Код купона")
+    code = forms.CharField(
+        label="Код купона",
+        widget=forms.TextInput(attrs={"placeholder": "Промо код"}),
+    )
