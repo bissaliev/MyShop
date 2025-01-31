db5b3e163651e0afaf9d9b830c96c89e2ab73f4b
bissaliev <bissaliev21@gmail.com>
2025-01-31 10:29:08 +0400

    orders: Регистрация в orders.urls  OrderUsersListView, OrderDetailView, OrderInvoiceView, OrderDeleteView.

--- myshop/orders/urls.py
+++ myshop/orders/urls.py
@@ -4,7 +4,19 @@ from . import views
 
 app_name = "orders"
 urlpatterns = [
+    path("", views.OrderUsersListView.as_view(), name="order_user_list"),
     path("create/", views.OrderCreateView.as_view(), name="order_create"),
+    path("<int:pk>/", views.OrderDetailView.as_view(), name="order_detail"),
+    path(
+        "<int:pk>/invoice/",
+        views.OrderInvoiceView.as_view(),
+        name="order_invoice",
+    ),
+    path(
+        "<int:pk>/delete/",
+        views.OrderDeleteView.as_view(),
+        name="order_delete",
+    ),
     # admin
     path(
         "admin/orders/<int:pk>/",
