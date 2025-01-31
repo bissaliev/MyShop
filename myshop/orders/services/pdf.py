e079844eed45e8b96eddd22f7e2b8215d0ad4575
bissaliev <bissaliev21@gmail.com>
2025-01-31 10:31:34 +0400

    orders: services/pdf.py добавил стили bootstrap.css

--- myshop/orders/services/pdf.py
+++ myshop/orders/services/pdf.py
@@ -9,7 +9,11 @@ from orders.models import Order
 def generate_invoice_pdf(order: Order):
     """Генерация PDF для счета-фактуры"""
     html = render_to_string("orders/order/pdf.html", {"order": order})
-    stylesheets = [weasyprint.CSS(settings.BASE_DIR / "static/css/pdf.css")]
+    stylesheets = [
+        weasyprint.CSS(settings.BASE_DIR / "static/css/pdf.css"),
+        weasyprint.CSS(settings.BASE_DIR / "static/css/bootstrap.min.css"),
+        weasyprint.CSS(settings.BASE_DIR / "static/css/custom.css"),
+    ]
     pdf_buffer = BytesIO()
     weasyprint.HTML(string=html).write_pdf(pdf_buffer, stylesheets=stylesheets)
     pdf_buffer.seek(0)
